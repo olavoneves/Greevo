44e0309de4824158c8eda81bed09d1bb746fb562
Olavo Neves <152447074+olavoneves@users.noreply.github.com>
2025-05-27 19:54:46 -0300

    Organiza main

--- Greevo_java/Greevo/src/br/com/fiap/greevo/main/GreevoMain.java
+++ Greevo_java/Greevo/src/br/com/fiap/greevo/main/GreevoMain.java
@@ -8,6 +8,10 @@ import java.awt.*;
 public class GreevoMain {
     public static void main(String[] args) {
         try {
+            String login;
+            String senha;
+            Administrador adminAtual;
+
             // Simula banco de dados
             Administrador admin1 = new Administrador("admin1", "senha1");
             Administrador admin2 = new Administrador("admin2", "senha2");
@@ -15,24 +19,23 @@ public class GreevoMain {
 
             int option = -1;
             while (option != 0) {
-                option = Integer.parseInt(JOptionPane.showInputDialog("""
-                        === GREEVO OPÇÕES ===
-                        
-                        1. Cadastrar Abrigos (Administrador)
-                        2. Visualizar Abrigos Próximos (Usuário)
-                        3. Controlar Lotação (Administrador)
-                        4. Verificar Alerta Meteorológico
-                        5. Falar com Chatbot
-                        0. Sair
-                        
-                        """, "Escolha uma opção:"));
+                option = Integer.parseInt(JOptionPane.showInputDialog(String.format(" === GREEVO OPÇÕES === \n\n 1. Cadastrar Abrigos (Administrador) \n 2. Visualizar Abrigos Próximos (Usuário) \n 3. Controlar Lotação (Administrador) \n 4. Verificar Alerta Meteorológico \n 5. Greevo Assistant \n\n 0. Sair"), "Escolha uma opção:"));
 
                 switch (option) {
                     case 1:
-                        String login = JOptionPane.showInputDialog("LOGIN:");
-                        String senha = JOptionPane.showInputDialog("SENHA: ");
+                        login = JOptionPane.showInputDialog("LOGIN:");
+                        senha = JOptionPane.showInputDialog("SENHA: ");
 
-                        if (admin1.autenticar(login, senha) || admin2.autenticar(login, senha) || admin3.autenticar(login, senha)) {
+                        adminAtual = null;
+                        if (admin1.autenticar(login, senha)) {
+                            adminAtual = admin1;
+                        } else if (admin2.autenticar(login, senha)) {
+                            adminAtual = admin2;
+                        } else if (admin3.autenticar(login, senha)) {
+                            adminAtual = admin3;
+                        }
+
+                        if (adminAtual != null) {
                             boolean cadastrando = true;
                             while (cadastrando) {
                                 String nomeAbrigo = JOptionPane.showInputDialog("--- Cadastro de Abrigo --- \nNome do Abrigo: ");
@@ -43,17 +46,14 @@ public class GreevoMain {
                                 boolean acessivel;
                                 if (possuiAcessibilidade == JOptionPane.YES_OPTION) {
                                     acessivel = true;
-                                } acessivel = false;
+                                } else {
+                                    acessivel = false;
+                                }
 
                                 try {
                                     Localizacao locAbrigo = new Localizacao(localAbrigo);
-                                    // Arrumar verificação de quem irá criar o novo abrigo
-                                    if (admin1) {
-                                        Abrigo newAbrigo = admin3.cadastrarAbrigo(nomeAbrigo, locAbrigo, capacidade, acessivel);
-                                    } else if (admin2) {
-                                        Abrigo newAbrigo = admin3.cadastrarAbrigo(nomeAbrigo, locAbrigo, capacidade, acessivel);
-                                    }
-                                    Abrigo newAbrigo = admin3.cadastrarAbrigo(nomeAbrigo, locAbrigo, capacidade, acessivel);
+                                    Abrigo newAbrigo = adminAtual.cadastrarAbrigo(nomeAbrigo, locAbrigo, capacidade, acessivel);
+                                    JOptionPane.showMessageDialog(null, "Abrigo cadastrado com sucesso!", "INFORMAÇÃO", JOptionPane.INFORMATION_MESSAGE);
                                 } catch (IllegalArgumentException e) {
                                     JOptionPane.showMessageDialog(null, e.getMessage(), "ERRO", JOptionPane.ERROR_MESSAGE);
                                 }
@@ -82,14 +82,16 @@ public class GreevoMain {
                             Usuario usuario = new Usuario(locUsuario, acessivel);
 
                             // Simula busca por abrigos próximos
-                            Abrigo newAbrigo = new Abrigo("Escola Municipal", new Localizacao("Teresópolis, RJ"), 100, true);
+                            Abrigo newAbrigo = new Abrigo("Escola Municipal", locUsuario, 100, true);
                             if (newAbrigo.inRaio(usuario.getLocalizacao(), 5.0)) {
                                 JOptionPane.showMessageDialog(null, String.format("--- Abrigo Disponível --- \n-" + newAbrigo.getNome() + " (" + newAbrigo.getLocalizacao().getNome() + ") \nVagas: " + (newAbrigo.getCapacidadeMaxima() - newAbrigo.getLotacaoAtual())), "INFORMAÇÃO", JOptionPane.INFORMATION_MESSAGE);
+                            } else {
+                                JOptionPane.showMessageDialog(null, String.format("Não existem refúgios em um raio de 5km de " + newAbrigo.getNome()));
                             }
 
                             boolean inAbrigos = true;
                             while (inAbrigos) {
-                                int subOpcao = Integer.parseInt(JOptionPane.showInputDialog("--- OPÇÕES --- \n1. Falar com Assistente Virtual \n0. Voltar ao Inicío \nEscolha a Opção:"));
+                                int subOpcao = Integer.parseInt(JOptionPane.showInputDialog("--- OPÇÕES --- \n1. Falar com Assistente Virtual \n\n0. Voltar ao Inicío \nEscolha a Opção:"));
 
                                 if (subOpcao == 1) {
                                     JOptionPane.showMessageDialog(null, "Entre na 5° Opção do menu principal!", "INFORMAÇÃO", JOptionPane.INFORMATION_MESSAGE);
@@ -106,7 +108,16 @@ public class GreevoMain {
                         login = JOptionPane.showInputDialog("LOGIN:");
                         senha = JOptionPane.showInputDialog("SENHA: ");
 
-                        if (admin1.autenticar(login, senha) || admin2.autenticar(login, senha) || admin3.autenticar(login, senha)) {
+                        adminAtual = null;
+                        if (admin1.autenticar(login, senha)) {
+                            adminAtual = admin1;
+                        } else if (admin2.autenticar(login, senha)) {
+                            adminAtual = admin2;
+                        } else if (admin3.autenticar(login, senha)) {
+                            adminAtual = admin3;
+                        }
+
+                        if (adminAtual != null) {
                             String nomeAbrigo = JOptionPane.showInputDialog("Nome do Abrigo: ");
                             int quantPessoas = Integer.parseInt(JOptionPane.showInputDialog("Quantidade de Pessoas: "));
 
@@ -114,8 +125,13 @@ public class GreevoMain {
                             Abrigo abrigo = new Abrigo(nomeAbrigo, new Localizacao("Teresópolis, RJ"), 100, true);
                             if (abrigo.adicionarPessoas(quantPessoas)) {
                                 JOptionPane.showMessageDialog(null, String.format("Lotação Atualizada: " + abrigo.getLotacaoAtual() + "/" + abrigo.getCapacidadeMaxima()));
-                            } JOptionPane.showMessageDialog(null, "Lotação Máxima! Sem Espaço", "INFORMAÇÃO", JOptionPane.INFORMATION_MESSAGE);
-                        } JOptionPane.showMessageDialog(null, "Acesso Negado", "ATENÇÃO", JOptionPane.WARNING_MESSAGE);
+                            } else {
+                                JOptionPane.showMessageDialog(null, "Lotação Máxima! Sem Espaço", "INFORMAÇÃO", JOptionPane.INFORMATION_MESSAGE);
+                            }
+
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Acesso Negado", "ATENÇÃO", JOptionPane.WARNING_MESSAGE);
+                        }
                         break;
                     case 4:
                         // Simulação de alerta, o ideal seria fazer para todos que tiverem login no banco de dados sem precisar pedir a localização
@@ -130,14 +146,38 @@ public class GreevoMain {
                         }
                         break;
                     case 5:
+                        ChatEmergencia chatEmergencia = new ChatEmergencia();
+                        ChatTurista chatTurista = new ChatTurista();
+
                         boolean inChatbot = true;
                         while (inChatbot) {
                             String mensagem = JOptionPane.showInputDialog("--- Greevo Assistant ---", "Buscando:");
 
                             if (mensagem.equalsIgnoreCase("sair")) {
                                 inChatbot = false;
+                            } else if (mensagem.contains("perigo") || mensagem.contains("ajuda") || mensagem.contains("obrigado") || mensagem.contains("alerta")) {
+                                JOptionPane.showMessageDialog(null, chatEmergencia.responder(mensagem), "INFORMAÇÃO", JOptionPane.INFORMATION_MESSAGE);
+                            } else if (mensagem.contains("idioma")) {
+                                int idioma = Integer.parseInt(JOptionPane.showInputDialog("Qual idioma você gostaria de se comunicar? \n 1. English \n 2. Spanish \n 3. Franch \n\n 0. Portuguese"));
+
+                                switch (idioma) {
+                                    case 1:
+                                        chatTurista.setIdioma("en");
+                                        break;
+                                    case 2:
+                                        chatTurista.setIdioma("es");
+                                        break;
+                                    case 3:
+                                        chatTurista.setIdioma("fr");
+                                        break;
+                                    case 0:
+                                        chatTurista.setIdioma("pt");
+                                        break;
+                                    default:
+                                        break;
+                                }
+                                JOptionPane.showMessageDialog(null, chatTurista.responder(mensagem), "INFORMAÇÃO", JOptionPane.INFORMATION_MESSAGE);
                             }
-                            // Adicionar regra de comunicação com Assistante
                         }
                         break;
                     case 0:
